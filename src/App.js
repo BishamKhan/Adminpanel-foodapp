ca8f4b2a474e2b163ad8e3176fd7484be29c3576
Maha Kaleem <mahakaleem15@gmail.com>
2022-10-24 01:35:13 +0500

    Dashboard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Dashboard from "./pages/Dashboard/Dashboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
 // import Menu from "./pages/Menu/menu";
 // import Restaurant from "./pages/Restaurants/index";
 // import Resprofile from "./pages/restaurants/profile/resprofile";
@@ -21,8 +21,8 @@ function App() {
         <NavBar />
         {/* <Resprofile/> */}
         <Routes>
-          {/* <Route path="/" element={<Dashboard />} /> */}
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           {/* <Route path="/menu" element={<Menu />} /> */}
           {/* <Route path="/restaurant" element={<Restaurant />} /> */}
           {/* <Route path="/orders" element={<Order/>} /> */}

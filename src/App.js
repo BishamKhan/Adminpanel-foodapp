b5304acbdbaeefa37872b38c3649e3656f99e67c
Maha Kaleem <mahakaleem15@gmail.com>
2022-10-24 01:31:25 +0500

    Sidebar-Navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Menu from "./pages/Menu/menu";
-import Restaurant from "./pages/Restaurants/index";
+// import Dashboard from "./pages/Dashboard/Dashboard";
+// import Menu from "./pages/Menu/menu";
+// import Restaurant from "./pages/Restaurants/index";
 // import Resprofile from "./pages/restaurants/profile/resprofile";
 import BarSide from "./Components/Bar-Side/BarSide";
 import NavBar from "./Components/NavBar/NavBar";
-import Addrestaurants from "./pages/Restaurants/Addrestaurant/addrestaurant";
-import Categories from "./Components/Menucard/Categories/categories";
-import Order from "./pages/Orders/Order";
+// import Addrestaurants from "./pages/Restaurants/Addrestaurant/addrestaurant";
+// import Categories from "./Components/Menucard/Categories/categories";
+// import Order from "./pages/Orders/Order";
 
 function App() {
   return (
@@ -21,13 +21,13 @@ function App() {
         <NavBar />
         {/* <Resprofile/> */}
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/restaurant" element={<Restaurant />} />
-          <Route path="/orders" element={<Order/>} />
-          <Route path="/addrestaurants" element={<Addrestaurants />} />
-          <Route path="/categories" element={<Categories/>} />
+          {/* <Route path="/" element={<Dashboard />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="/menu" element={<Menu />} /> */}
+          {/* <Route path="/restaurant" element={<Restaurant />} /> */}
+          {/* <Route path="/orders" element={<Order/>} /> */}
+          {/* <Route path="/addrestaurants" element={<Addrestaurants />} /> */}
+          {/* <Route path="/categories" element={<Categories/>} /> */}
         </Routes>
       </div>
     </BrowserRouter>
